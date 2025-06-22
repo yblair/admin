4f0d6582da8ce11407d1642939a80e816677471d
ylair <lair.yamila.belen@gmail.com>
2025-06-21 23:23:07 -0300

    fix: theme palette and variables

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -4,25 +4,37 @@ import {
   ChevronLeftIcon,
 } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 import styles from "./sidebar.module.css";
 import clsx from "clsx";
 
 export const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const location = useLocation();
+
+  const isHomeActive =
+    location.pathname === "/" || location.pathname.startsWith("/orders");
 
   return (
     <nav className={clsx(styles.sidebar, { [styles.collapsed]: collapsed })}>
       <ul>
         <li>
-          <NavLink to={`/`} className={styles.navigation}>
+          <NavLink
+            to={`/`}
+            className={isHomeActive ? styles.active : styles.inactive}
+          >
             <HomeIcon />
             <span> Home</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to={`/messages`} className={styles.navigation}>
+          <NavLink
+            to={`/messages`}
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.inactive
+            }
+          >
             <EnvelopeIcon />
             <span> Messages</span>
           </NavLink>
