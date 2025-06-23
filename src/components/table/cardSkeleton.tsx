6c78bae414b0f81f7160944a5680b1742c65a717
ylair <lair.yamila.belen@gmail.com>
2025-06-23 16:55:01 -0300

    fix: width skeleton

--- src/components/table/cardSkeleton.tsx
+++ src/components/table/cardSkeleton.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import styles from "../card/cardSkeleton.module.css";
+import styles from "./cardSkeleton.module.css";
 
 const CardSkeleton: React.FC = () => (
   <div className={styles.mobileCardSkeleton}>
     <div className={styles.mobileCardHeader}>
       <div className={styles.mobileCardCustomer}>
         <div className={styles.skeletonLine + " " + styles.skeletonName} />
-        <div className={styles.skeletonLine + " " + styles.skeletonEmail} />
       </div>
       <div className={styles.skeletonStatus} />
     </div>
+    <div className={styles.skeletonLine + " " + styles.skeletonEmail} />
     <div className={styles.mobileCardDetails}>
       <div className={styles.mobileCardRow}>
         <div className={styles.skeletonLine + " " + styles.skeletonLabel} />
